f36521a7b36524d65bbc188a0f4d8ec90daa93a0
André Obelink <andre@obelink.com>
2017-10-23 23:19:02 +0200

    Initial commit

---
+++ MainForm.Designer.vb
@@ -0,0 +1,67 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class MainForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.JsonResultTextBox = New System.Windows.Forms.TextBox()
+        Me.SelectFileButton = New System.Windows.Forms.Button()
+        Me.SuspendLayout
+        '
+        'JsonResultTextBox
+        '
+        Me.JsonResultTextBox.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom)  _
+            Or System.Windows.Forms.AnchorStyles.Left)  _
+            Or System.Windows.Forms.AnchorStyles.Right),System.Windows.Forms.AnchorStyles)
+        Me.JsonResultTextBox.Location = New System.Drawing.Point(24, 22)
+        Me.JsonResultTextBox.Multiline = true
+        Me.JsonResultTextBox.Name = "JsonResultTextBox"
+        Me.JsonResultTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.JsonResultTextBox.Size = New System.Drawing.Size(1256, 735)
+        Me.JsonResultTextBox.TabIndex = 0
+        '
+        'SelectFileButton
+        '
+        Me.SelectFileButton.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right),System.Windows.Forms.AnchorStyles)
+        Me.SelectFileButton.Location = New System.Drawing.Point(1092, 774)
+        Me.SelectFileButton.Name = "SelectFileButton"
+        Me.SelectFileButton.Size = New System.Drawing.Size(188, 43)
+        Me.SelectFileButton.TabIndex = 1
+        Me.SelectFileButton.Text = "Select file..."
+        Me.SelectFileButton.UseVisualStyleBackColor = true
+        '
+        'MainForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(12!, 25!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1300, 829)
+        Me.Controls.Add(Me.SelectFileButton)
+        Me.Controls.Add(Me.JsonResultTextBox)
+        Me.Name = "MainForm"
+        Me.Text = "Demo"
+        Me.ResumeLayout(false)
+        Me.PerformLayout
+
+End Sub
+
+    Friend WithEvents JsonResultTextBox As TextBox
+    Friend WithEvents SelectFileButton As Button
+End Class
---
+++ MainForm.vb
@@ -0,0 +1,75 @@
+﻿Imports System.IO
+Imports System.Net.Http
+Imports System.Net.Http.Headers
+Imports JsonPrettyPrinterPlus.PrettyPrinterExtensions
+
+Public Class MainForm
+
+    Private ReadOnly _subscriptionKey = "17e72298c40a4532901b20c1df72c5b2"
+    Private ReadOnly _uriBase = "https://westeurope.api.cognitive.microsoft.com/vision/v1.0/"
+
+    Private Async Function GetAnalyzeImageResultAsync(imageFilePath As String) As Task(Of String)
+
+        Dim imageAsBytes = GetImageAsByteArray(imageFilePath)
+        Dim requestUri = GetRequestUri()
+
+        using client As New HttpClient()
+            client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", _subscriptionKey)
+
+            using content = new ByteArrayContent(imageAsBytes)
+                content.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream")
+                Dim response = await client.PostAsync(requestUri, content)
+                return await response.Content.ReadAsStringAsync()
+            End Using
+               
+        End Using
+
+    End Function
+
+    Private Function GetImageAsByteArray(imageFilePath As String) As Byte()
+
+        Using fileStream As New FileStream(imageFilePath, FileMode.Open, FileAccess.Read)
+            Using binaryReader As New BinaryReader(fileStream)
+                Return binaryReader.ReadBytes(fileStream.Length)
+            End Using
+        End Using
+
+    End Function
+
+    Private Function GetImageFilePath() As String
+
+        Dim dialog As New OpenFileDialog
+        With dialog
+            .Filter = "jpg files (*.jpg)|*.jpg|All files (*.*)|*.*"
+
+            If .ShowDialog = DialogResult.OK Then
+                Return .FileName
+            Else
+                Return String.Empty
+            End If
+
+        End With
+  
+    End Function
+
+    Private Function GetRequestUri() As String
+
+        Const operation = "analyze/"
+        Const requestParameters = "visualFeatures=Categories,Tags,Description"
+
+        Return _uriBase & operation & "?" & requestParameters
+
+    End Function
+    
+    Private Async Sub SelectFileButton_Click(sender As Object, e As EventArgs) Handles SelectFileButton.Click
+
+        Dim imageFilePath = GetImageFilePath()
+
+        If imageFilePath <> String.Empty Then
+            Dim jsonResult = Await GetAnalyzeImageResultAsync(imageFilePath)
+            JsonResultTextBox.Text = jsonResult.PrettyPrintJson()
+        End If
+
+    End Sub
+    
+End Class
